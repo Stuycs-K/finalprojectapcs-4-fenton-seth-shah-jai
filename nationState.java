e2631cf9f5432fa7b97eab44e12b4bc1ff61c17d
Seth Fenton <safenton08@gmail.com>
2025-05-22 20:44:03 -0400

    nationstate now both functions, and is updated with taxBracket management, need to manage gdpGrowthRate equation now

--- nationState.java
+++ nationState.java
@@ -3,28 +3,44 @@ public class nationState {
   //I think we have to do an ArrayListOfBonds
  private static double globalGDP, GEGM, GlobalGDPGrowth, year;
  //including no way to modify net exports for now but that will change
- private double consumerSpending, Investment, GovernmentSpending, Exports, Imports, DefaultGDPGrowthRate, interestRate, inflationRate,nationalSpending, taxRevenue, nationalDebt;
+ private double consumerSpending, Investment, GovernmentSpending, Exports, Imports, GDPGrowthRate, interestRate, inflationRate,nationalSpending, taxRevenue, nationalDebt, maxTaxBracket, oldTaxBracket;
  private ArrayList<Bonds> listOfBonds;
 //government budget breaks down into six core sectors, manufacturing (standin for most subsidies), non healthcare welfare, healthcare, salaries (standin for most pensions and labor expenditures), millitary, anythingElse
 //means every array will range from index 0 to index 6
 private double[] govBudgetBreakDown;
 
-public nationState(double ConsumerSpending, double Investment, double GovernmentSpending, double Exports, double Imports, double DefaultGDPGrowthRate, double interestRate, double inflationRate, double[] govBudgetBreakDown, double taxRevenue, double nationalDebt) {
+public nationState(double ConsumerSpending, double Investment, double GovernmentSpending, double Exports, double Imports, double DefaultGDPGrowthRate, double interestRate, double inflationRate, double[] govBudgetBreakDown, double taxRevenue, double nationalDebt, double maxTaxBracket) {
   consumerSpending = ConsumerSpending;
   Investment = Investment;
   GovernmentSpending = GovernmentSpending;
   Exports = Exports;
   Imports = Imports;
-  DefaultGDPGrowthRate = DefaultGDPGrowthRate;
+  GDPGrowthRate = DefaultGDPGrowthRate;
   interestRate = interestRate;
   inflationRate = inflationRate;
   govBudgetBreakDown = govBudgetBreakDown;
   taxRevenue = taxRevenue;
   listOfBonds = new ArrayList<Bonds>(0);
+  maxTaxBracket = maxTaxBracket;
 
 
 
 }
+
+public void updateTaxRevenue() {
+taxRevenue = (1 + GDPGrowthRate) * taxRevenue * maxTaxBracket / oldTaxBracket;
+oldTaxBracket = maxTaxBracket;
+}
+
+public void setMaxBracket(double newMaxBracket) {
+  oldTaxBracket = maxTaxBracket;
+  maxTaxBracket = newMaxBracket;
+}
+
+public double netExports() {
+return Exports - Imports;
+}
+
 
 public double findBudgetBalence() {
  return taxRevenue - GovernmentSpending;
