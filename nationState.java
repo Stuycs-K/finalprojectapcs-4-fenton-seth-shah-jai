7ebd5b1415f44a082851c2ec8427a189fe815768
Seth04268 <sfenton50@stuy.edu>
2025-05-27 11:02:18 -0400

    began working on governement sector gdp change, once done pathetically easy to design equation then work backwards

--- nationState.java
+++ nationState.java
@@ -14,6 +14,7 @@ public nationState(double ConsumerSpending, double Investment, double Government
   Investment = Investment;
   Exports = Exports;
   Imports = Imports;
+ private ArrayList<Bonds> listOfB
   GDPGrowthRate = DefaultGDPGrowthRate;
   interestRate = interestRate;
   inflationRate = inflationRate;
@@ -90,17 +91,26 @@ public double calcInflation(double oldTaxRevenue, double oldInterestRate, double
   return (oldTaxRevenue / taxRevenue) * .3 + (interestRate + 1) / (oldInterestRate + 1) * .3 + (governmentSpending() / oldGovSpending) * .4);
 }
 
-public double calcGDPGrowthBySector() {
+public double calculateGDPGrowthForOneDollarUnitOneSector  (double initialCoefficent, double totalPercentChange) {
+return initialCoefficent / (totalPercentChange + 1);
+}
+
+public double calcGDPGrowthBySector(double[] oldGovernmentBudget) {
+  //government budget breaks down into six core sectors, manufacturing (standin for most subsidies), non healthcare welfare, healthcare, salaries (standin for most pensions and labor expenditures), millitary, anythingElse
+
 
 }
-}
+
 
 public double calculateGDPGrowth(double[] governmentBudget, double newinterestRate, double newMaxBracket) {
   oldTaxRevenue = updateTaxRevenue(newMaxBracket);
   oldGovernmentBudget = setGovernmentSpending(governmentBudget);
   oldInterestRate = setInterestRate(newinterestRate);
-  for (int i = 0; i <)
-
+  double oldTotalGovernmentSpending = 0;
+  for (int i = 0; i < oldGovernmentBudget.length; i++) {
+oldTotalGovernmentSpending += oldGovernmentBudget[i];
+  }
+  calcGDPGrowthBySector()
   return
 }
 
