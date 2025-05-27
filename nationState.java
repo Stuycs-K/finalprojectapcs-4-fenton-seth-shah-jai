3fce6a33fc46cb3ca0270383ce2a16f847d2b305
Seth04268 <sfenton50@stuy.edu>
2025-05-27 10:47:36 -0400

    pushing to main so Jai can see more accurately how the code is shaping up even though it's not done

--- nationState.java
+++ nationState.java
@@ -3,7 +3,7 @@ public class nationState {
   //I think we have to do an ArrayListOfBonds
  private static double globalGDP, GEGM, GlobalGDPGrowth, year;
  //including no way to modify net exports for now but that will change
- private double consumerSpending, Investment, GovernmentSpending, Exports, Imports, GDPGrowthRate, interestRate, inflationRate,nationalSpending, taxRevenue, nationalDebt, maxTaxBracket, oldTaxBracket;
+ private double consumerSpending, Investment, Exports, Imports, GDPGrowthRate, interestRate, inflationRate, taxRevenue, nationalDebt, maxTaxBracket, joy;
  private ArrayList<Bonds> listOfBonds;
 //government budget breaks down into six core sectors, manufacturing (standin for most subsidies), non healthcare welfare, healthcare, salaries (standin for most pensions and labor expenditures), millitary, anythingElse
 //means every array will range from index 0 to index 6
@@ -12,7 +12,6 @@ private double[] govBudgetBreakDown;
 public nationState(double ConsumerSpending, double Investment, double GovernmentSpending, double Exports, double Imports, double DefaultGDPGrowthRate, double interestRate, double inflationRate, double[] govBudgetBreakDown, double taxRevenue, double nationalDebt, double maxTaxBracket) {
   consumerSpending = ConsumerSpending;
   Investment = Investment;
-  GovernmentSpending = GovernmentSpending;
   Exports = Exports;
   Imports = Imports;
   GDPGrowthRate = DefaultGDPGrowthRate;
@@ -22,6 +21,7 @@ public nationState(double ConsumerSpending, double Investment, double Government
   taxRevenue = taxRevenue;
   listOfBonds = new ArrayList<Bonds>(0);
   maxTaxBracket = maxTaxBracket;
+  joy = joy;
 
 
 
@@ -35,29 +35,40 @@ return oldTaxRevenue;
 
 }
 
+public double getJoy() {
+  return joy;
+}
+
 public double setMaxBracket(double newMaxBracket) {
   oldTaxBracket = maxTaxBracket;
   maxTaxBracket = newMaxBracket;
   return oldTaxBracket;
 }
 
+public double governmentSpending() {
+  double govSpendingSum = 0;
+  for (int i = 0; i < govBudgetBreakDown.length; i++) {
+    govSpendingSum += govBudgetBreakDown[i];
+  }
+ return govSpendingSum;
+}
 public double netExports() {
 return Exports - Imports;
 }
 
 
 public double findBudgetBalence() {
- return taxRevenue - GovernmentSpending;
+ return taxRevenue - governmentSpending();
 }
 
 public double[] setGovernmentSpending(double[] govSpending) {
-oldGovSpending = GovernmentSpending;
-GovernmentSpending = govSpending;
+oldGovSpending = govBudgetBreakDown;
+govBudgetBreakDown = govSpending;
 return oldGovSpending;
-}														
+}
 
 public double GDP() {
-  return GovernmentSpending + (Exports - Imports) + Investment + consumerSpending;
+  return governmentSpending() + (Exports - Imports) + Investment + consumerSpending;
 }
 
 public double RealGDP() {
@@ -72,9 +83,26 @@ public double getTaxRevenue() {
   return taxRevenue;
 }
 public double getSpending() {
-  return GovernmentSpending;
+  return governmentSpending();
 }
 
+public double calcInflation(double oldTaxRevenue, double oldInterestRate, double oldGovSpending) {
+  return (oldTaxRevenue / taxRevenue) * .3 + (interestRate + 1) / (oldInterestRate + 1) * .3 + (governmentSpending() / oldGovSpending) * .4);
+}
+
+public double calcGDPGrowthBySector() {
+
+}
+}
+
+public double calculateGDPGrowth(double[] governmentBudget, double newinterestRate, double newMaxBracket) {
+  oldTaxRevenue = updateTaxRevenue(newMaxBracket);
+  oldGovernmentBudget = setGovernmentSpending(governmentBudget);
+  oldInterestRate = setInterestRate(newinterestRate);
+  for (int i = 0; i <)
+
+  return
+}
 
 
 
