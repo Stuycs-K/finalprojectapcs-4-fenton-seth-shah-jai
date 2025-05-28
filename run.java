e7538a5bf0ef7b19401577a9becb0a1b37ba96e5
Jai Shah <jais5@nycstudents.net>
2025-05-27 23:28:32 -0400

    Finished run, just need handling for bad inputs

--- run.java
+++ run.java
@@ -1,16 +1,57 @@
 import java.util.*;
 public class run{
   private String userIn;
+  private Double budgetNum;
   private nationState nation;
-  nationState Germany=new nationState(0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0);
+  nationState Germany=new nationState(0, 0, 0, 0, 0, 0, 0, new double[]{0, 0, 0, 0, 0, 0}, 0, 0, 0);
 
 
-  public GrabUserInput(){
+  public void GrabUserInput(){
     Scanner read=new Scanner(System.in);
-    System.out.println("PLACEHOLDER FOR INPUT");
     userIn=read.nextLine();
   }
+  public void GrabUserNum(){
+    Scanner read=new Scanner(System.in);
+    budgetNum=read.nextDouble();
+  }
   public void ModifyNations(){
+    System.out.println("And how much?");
+    GrabUserNum();
+    if(budgetNum==0){
+      System.out.println("Nope, its 1");
+      budgetNum=1;
+    }
+    if(userIn.equals("Taxes")){
+      nation.updateTaxRevenue(budgetNum);
+    }
+    else if(userIn.equals("Interest")){
+      nation.setInterestRate(budgetNum);
+    }
+    else if(userIn.equals("Budget")){
+      System.out.println("Which line item? Manufacturing, Welfare, Healthcare, Salaries, Military, Other?");
+      GrabUserInput();
+      double bud=nation.getGovBudgetBreakDown();
+      if(userIn.equals("Manufacturing")){
+        bud[0]+=budgetNum;
+      }
+      else if(userIn.equals("Welfare")){
+        bud[1]+=budgetNum;
+      }
+      else if(userIn.equals("Healthcare")){
+        bud[2]+=budgetNum;
+      }
+      else if(userIn.equals("Salaries")){
+        bud[3]+=budgetNum;
+      }
+      else if(userIn.equals("Military")){
+        bud[4]+=budgetNum;
+      }
+      else if(userIn.equals("Other")){
+        bud[5]+=budgetNum;
+      }
+      nation.setGovernmentSpending(bud);
+
+    }
     
   }
   public boolean initializeNation(){
@@ -38,7 +79,9 @@ public class run{
       }
   }
   public static void main(String[] args) {
-    run();
+    run newSim=new run();
+    sim.run();
   }
     //Take the function for calculateGDPgrowth and use use userinputs
+    //Add handling for bad inputs
 }
